4b8381944d6a32e551e17eb4debdbc9d86801e52
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-03-14 12:22:41 +0530

    contact-header

---
+++ front_end/src/components/ContactHeader.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../styles/HomeScreen.css';
+
+export default function ContactHeader() {
+  return (
+    <section className="contact-header">
+      <div className="logo-container">Machine</div>
+      <div className="contact-detail-container">
+        <div className="contact-container">
+          <div className="contact-logo-container">
+            <i className="fa-solid fa-phone-volume"></i>
+          </div>
+          <div className="contact-logo-detail-container">
+            <h3>Phone</h3>
+            +91 63698 38362
+          </div>
+        </div>
+        <div className="contact-container">
+          <div className="contact-logo-container">
+            <i className="fa-solid fa-house"></i>
+          </div>
+          <div className="contact-logo-detail-container">
+            <h3>Address</h3>
+            125, Gandhi Nagar, Chennai - 600008.
+          </div>
+        </div>
+        <div className="contact-container">
+          <div className="contact-logo-container">
+            <i className="fa-regular fa-clock"></i>
+          </div>
+          <div className="contact-logo-detail-container">
+            <h3>Office Hours</h3>
+            8:30 am - 7:00 pm
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- front_end/src/screens/HomeScreen.js
+++ front_end/src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import ContactHeader from '../components/ContactHeader';
 import PageHeader from '../components/PageHeader';
 
 export default function HomeScreen() {
   return (
     <section className="home-page">
       <PageHeader />
+      <ContactHeader />
     </section>
   );
 }
