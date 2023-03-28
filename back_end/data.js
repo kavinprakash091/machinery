f69d21553c00af9c0132b1ad9d7558a912615dee
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-03-28 12:56:08 +0530

    Updated-machine-screen

---
+++ back_end/data.js
@@ -0,0 +1,9 @@
+const data = {
+  products: [
+    { name: [Kavin, Janu] },
+    { age: [22, 19] },
+    { gender: [male, female] },
+  ],
+};
+
+export default data;
---
+++ back_end/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import core from 'cors';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to db');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+app.use(core());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../front_end/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../front_end/build/index.html'));
+});
