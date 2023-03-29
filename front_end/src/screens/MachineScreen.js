10a876211d168f8b8be8cc0c0d5c78e2afd440e1
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-03-29 22:08:37 +0530

    backend-machinescreen

--- front_end/src/screens/MachineScreen.js
+++ front_end/src/screens/MachineScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 import ContactHeader from '../components/ContactHeader';
@@ -8,17 +8,21 @@ import PageHeader from '../components/PageHeader';
 import ProductLists from '../components/ProductLists';
 import Sidebar from '../components/Sidebar';
 import data from '../data';
+import { Store } from '../Store';
 import '../styles/MachineScreen.css';
 
 export default function MachineScreen() {
+  const { state, ctxDispatch: dispatch } = useContext(Store);
+  const { machine_list } = state;
   const { machine_name } = useParams();
   var product_name;
   machine_name && (product_name = machine_name.replace(/-/g, ' '));
 
   var product_index = 0;
   machine_name &&
-    data.products.map((product, index) => {
-      product.toLowerCase() === product_name && (product_index = index);
+    machine_list &&
+    machine_list.map((product, index) => {
+      product.name.toLowerCase() === product_name && (product_index = index);
     });
 
   return (
@@ -33,10 +37,10 @@ export default function MachineScreen() {
       <Sidebar />
       <div className="machines-container">
         <div className="machine-header-container">
-          <h1> {data.products[product_index]} </h1>
-          <p>{data.product_description[product_index]}</p>
+          <h1> {machine_list[product_index].name} </h1>
+          <p>{machine_list[product_index].description}</p>
         </div>
-        <ProductLists machines={data.product_details[product_index]} />
+        <ProductLists machines={machine_list[product_index].product_details} />
       </div>
     </section>
   );
