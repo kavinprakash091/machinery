61d9235a87b52416acb9a246dfbeb76e3719e764
kavinprakash091 <kavinprakashofficial@gmail.com>
2023-04-24 00:29:39 +0530

    hosting

--- back_end/server.js
+++ back_end/server.js
@@ -38,7 +38,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../front_end/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
